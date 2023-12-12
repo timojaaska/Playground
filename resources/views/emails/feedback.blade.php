04574e572870027b3946c43c801c7baa54bd2d59
Timo Jääskä <timo@jaaska.fi>
2023-12-12 22:34:34 +0200

    feedback update

--- resources/views/emails/feedback.blade.php
+++ resources/views/emails/feedback.blade.php
@@ -1,5 +1,7 @@
 <div>
+    <h3>Lähettäjän tiedot</h3>
     <p><strong>Nimi:</strong> {{ $feedbackData['name'] }}</p>
     <p><strong>Sähköposti:</strong> {{ $feedbackData['email'] }}</p>
-    <p><strong>Palaute:</strong> {{ $feedbackData['feedback'] }}</p>
+    <h3>Palaute:</h3>
+    <p>{{ $feedbackData['feedback'] }}</p>
 </div>
