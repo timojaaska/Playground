e5793923bf9a1490aa39911d566c5edbc518f711
Toni Lahnalampi <toni.lahnalampi@codetag.fi>
2024-01-18 10:46:31 +0200

    add modal.vue

---
+++ resources/js/components/common/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="modal fade"
+    :class="[{ show: show }, sizeClass]"
+    tabindex="-1"
+    role="dialog"
+    aria-hidden="true"
+    :style="{display: show ? 'block': 'none'}"
+    @click.stop="$emit('close')"
+  >
+    <div class="modal-dialog" role="document" @click.stop>
+      <div class="modal-content">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    },
+    variant: {
+      type: String,
+      default: ''
+    }
+  },
+  emits: ['close'],
+  computed: {
+    sizeClass() {
+      if (this.variant) {
+        return `modal-${this.variant}`
+      }
+      return ''
+    }
+  },
+}
+</script>
+
+<style scoped>
+.modal.fade {
+  background: rgba(0,0,0,0.50);
+  backdrop-filter: blur(6px);
+  -webkit-backdrop-filter: blur(6px);
+}
+</style>
